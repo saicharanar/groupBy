255f5ec70f534a3576ade5f94d2df8158966f469
Azhar Beg <99490876+azhar-beg@users.noreply.github.com>
2022-04-06 09:33:45 +0530

    Completed code for nested array elements.

--- groupBy.js
+++ groupBy.js
@@ -1,6 +1,6 @@
 const assertLib = require('./assert.js');
 const assert = assertLib.assert;
-const areEqual = assertLib.areEqual;
+const isEqual = assertLib.isEqual;
 
 const doesIncludes = function (array, element) {
   if (!Array.isArray(element)) {
@@ -8,7 +8,7 @@ const doesIncludes = function (array, element) {
   }
 
   for (let index = 0; index < array.length; index++) {
-    if (areEqual(element, array[index])) {
+    if (isEqual(element, array[index])) {
       return true;
     }
   }
@@ -25,26 +25,26 @@ const uniqueElements = function (array) {
   return unique;
 };
 
-// const elementGroup = function (element, array) {
-//   const group = [];
-//   for (let index = 0; index < array.length; index++) {
-//     if (areEqual(element, array[index])) {
-//       group.push(array[index]);
-//     }
-//   }
-//   return group;
-// }
+const elementGroup = function (element, array) {
+  const group = [];
+  for (let index = 0; index < array.length; index++) {
+    if (isEqual(element, array[index])) {
+      group.push(array[index]);
+    }
+  }
+  return group;
+}
 
-// const groupSameElements = function (array) {
-//   const unique = uniqueElements(array);
-//   const groupedElements = [];
-//   for (let index = 0; index < unique.length; index++) {
-//     groupedElements.push(elementGroup(unique[index], array));
-//   }
-//   return groupedElements;
-// };
+const groupSameElements = function (array) {
+  const unique = uniqueElements(array);
+  const groupedElements = [];
+  for (let index = 0; index < unique.length; index++) {
+    groupedElements.push(elementGroup(unique[index], array));
+  }
+  return groupedElements;
+};
 
-const testElementsFrequency = function (array, expected, message) {
+const testElementGrouping = function (array, expected, message) {
   const actual = groupSameElements(array);
   console.log(actual);
   console.log(expected);
@@ -52,19 +52,17 @@ const testElementsFrequency = function (array, expected, message) {
 };
 
 const elementGroupingTestCases = function () {
-  testElementsFrequency([1], [[1, 1]], 'A single element');
-  // testElementsFrequency([[1]], [[[1]]], 'A single element as array');
-  // testElementsFrequency([1, [1], [1]], [[1], [[1], [1]]], 'An array with numbers and an array');
-  // testElementsFrequency([1, 2], [[1], [2]], 'An array with only numbers');
-  // testElementsFrequency([1, 2, 1], [[1, 1], [2]], 'An array with only numbers');
-  // testElementsFrequency([1, 2, [1]], [[1], [2], [[1]]], 'An array with numbers and an array');
-  // testElementsFrequency([1, 2, [1, 1]], [[1], [2], [[1, 1]]], 'An array with numbers and an array');
-  // testElementsFrequency([1, 2, [1, 1], [1, 1]], [[1], [2], [[1, 1], [1, 1]]], 'An array with numbers and an array');
-  // testElementsFrequency([1, 2, 1], [[1, 1], [2]], 'An array with numbers and an array');
-  // testElementsFrequency([1, 2, 3, 1, 2, 4], [[1, 1], [2, 2], [3], [4]], 'An array with numbers and an array');
-  // testElementsFrequency([[1, 1], 1, [1, 1], 1], [[[1, 1], [1, 1]], [1, 1]], 'An array with numbers and an array');
+  testElementGrouping([1], [[1]], 'A single element');
+  testElementGrouping([[1]], [[[1]]], 'A single element as array');
+  testElementGrouping([1, [1], [1]], [[1], [[1], [1]]], 'An array with numbers and an array');
+  testElementGrouping([1, 2], [[1], [2]], 'An array with only numbers');
+  testElementGrouping([1, 2, 1], [[1, 1], [2]], 'An array with only numbers');
+  testElementGrouping([1, 2, [1]], [[1], [2], [[1]]], 'An array with numbers and an array');
+  testElementGrouping([1, 2, [1, 1]], [[1], [2], [[1, 1]]], 'An array with numbers and an array');
+  testElementGrouping([1, 2, [1, 1], [1, 1]], [[1], [2], [[1, 1], [1, 1]]], 'An array with numbers and an array');
+  testElementGrouping([1, 2, 1], [[1, 1], [2]], 'An array with numbers and an array');
+  testElementGrouping([1, 2, 3, 1, 2, 4], [[1, 1], [2, 2], [3], [4]], 'An array with numbers and an array');
+  testElementGrouping([[1, 1], 1, [1, 1], 1], [[[1, 1], [1, 1]], [1, 1]], 'An array with numbers and an array');
 };
 
 elementGroupingTestCases();
-// console.log(groupSameElements([[[1], [2]], [[1], [2]], 1]));
-// console.log(groupSameElements([[[1], [2]], [[1], [2]]]));
